ce4817494b61e58fd65eac07b7a7145292f91efd
Ilya misyura <ilya.mi@digitrade.co.il>
2022-12-09 14:25:56 +0200

    add

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import { useTelegram } from './hooks/useTelegram';
-
+import { Route, Routes } from 'react-router-dom';
+import ProductList from './components/ProductList/ProductList';
+import Form from './components/Form/Form';
 function App() {
 
 
@@ -16,7 +18,11 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <button onCanPlay={onToggleButton}>toggle</button>
+      <Routes>
+        <Route index element={<ProductList/>}/>
+        <Route path={'/form'} element={<Form/>}/>
+      </Routes>
+      {/* <button onCanPlay={onToggleButton}>toggle</button> */}
     </div>
   );
 }
