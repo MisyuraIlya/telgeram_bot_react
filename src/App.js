6cdfdd8e3e076bdc33d708e0afc59f12ba9f5b06
Ilya misyura <ilya.mi@digitrade.co.il>
2022-12-09 14:19:20 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 import { useTelegram } from './hooks/useTelegram';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
 
   return (
     <div className="App">
+      <Header/>
       <button onCanPlay={onToggleButton}>toggle</button>
     </div>
   );
